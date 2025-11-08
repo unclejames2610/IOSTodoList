a36cca10972d582926522a8eb819a4495662fc97
unclejames2610 <udealaebuka@gmail.com>
2025-11-08 06:33:51 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ebuka on 08/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
